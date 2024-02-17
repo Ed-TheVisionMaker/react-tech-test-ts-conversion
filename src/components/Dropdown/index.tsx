c6650a4b12d38774ba8c679084e016b00bbdfccd
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-02-17 10:20:06 +0000

    style and position dropdown

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,25 +1,30 @@
 import React, { ChangeEvent } from 'react';
+import './DropdownMenu.css'
 
 interface DropdownProps {
   handleChange: (e: ChangeEvent<HTMLSelectElement>) => void;
   numberOfDrinks: number;
 }
 
-const Dropdown: React.FC<DropdownProps> = ({ handleChange, numberOfDrinks }) => {
-
+const Dropdown: React.FC<DropdownProps> = ({
+  handleChange,
+  numberOfDrinks,
+}) => {
   const preventRefresh = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
   };
 
   return (
-    <form onSubmit={preventRefresh}>
-      <label>Drinks Per Page</label>
-      <select name='number' value={numberOfDrinks} onChange={handleChange}>
-        <option value='10'>10</option>
-        <option value='20'>20</option>
-        <option value='30'>30</option>
-      </select>
-    </form>
+    <div className={'dropdown-container'} >
+      <form onSubmit={preventRefresh}>
+        <label className={'dropdown-dropdown-label'}>Drinks Per Page</label>
+        <select className={'dropdown-dropdown-select'} name='number' value={numberOfDrinks} onChange={handleChange}>
+          <option value='10'>10</option>
+          <option value='20'>20</option>
+          <option value='30'>30</option>
+        </select>
+      </form>
+    </div>
   );
 };
 
